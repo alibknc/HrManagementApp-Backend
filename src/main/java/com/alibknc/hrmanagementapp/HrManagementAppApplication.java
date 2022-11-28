6c4ca38b823847c17dc69413568ae64bd9f699e0
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-11-28 22:29:51 +0300

    Initial commit

---
+++ src/main/java/com/alibknc/hrmanagementapp/HrManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.alibknc.hrmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alibknc/hrmanagementapp/HrManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibknc.hrmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
