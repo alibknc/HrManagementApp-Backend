8f28f4cea9cc61cffef53613ef5ae15409715280
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-11-30 22:59:03 +0300

    ExpenseService class created

---
+++ src/main/java/com/alibknc/hrmanagementapp/service/ExpenseService.java
@@ -0,0 +1,27 @@
+package com.alibknc.hrmanagementapp.service;
+
+import com.alibknc.hrmanagementapp.client.dto.response.ExpenseDto;
+import com.alibknc.hrmanagementapp.mapper.ExpenseMapper;
+import com.alibknc.hrmanagementapp.model.Expense;
+import com.alibknc.hrmanagementapp.repository.ExpenseRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+    private final ExpenseMapper expenseMapper;
+
+    public List<ExpenseDto> getAllExpenses() {
+        List<Expense> expenseList = expenseRepository.findAll();
+
+        return expenseList.stream()
+                .map(expenseMapper::toExpenseDto)
+                .collect(Collectors.toList());
+    }
+}
