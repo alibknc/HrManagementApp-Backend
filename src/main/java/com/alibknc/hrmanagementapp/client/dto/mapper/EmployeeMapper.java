e6ac0012d3313d8e53bd250232c8b9f5f48c18dd
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-09 14:46:04 +0300

    toEmployee added to EmployeeMapper

--- src/main/java/com/alibknc/hrmanagementapp/client/dto/mapper/EmployeeMapper.java
+++ src/main/java/com/alibknc/hrmanagementapp/client/dto/mapper/EmployeeMapper.java
@@ -3,9 +3,12 @@ package com.alibknc.hrmanagementapp.client.dto.mapper;
 import com.alibknc.hrmanagementapp.client.dto.response.EmployeeDto;
 import com.alibknc.hrmanagementapp.data.entity.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(implementationName = "EmployeeMapperImpl", componentModel = "spring")
+@Mapper(implementationName = "EmployeeMapperImpl", componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EmployeeMapper {
 
     EmployeeDto toEmployeeDto(Employee employee);
+
+    Employee toEmployee(EmployeeDto employeeDto);
 }
