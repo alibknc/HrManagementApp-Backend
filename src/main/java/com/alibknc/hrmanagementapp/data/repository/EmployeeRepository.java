d82bf088784c688465481e10fbcdf7d5292842cb
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-09 14:47:27 +0300

    findAllEmployees query added to EmployeeRepository

--- src/main/java/com/alibknc/hrmanagementapp/data/repository/EmployeeRepository.java
+++ src/main/java/com/alibknc/hrmanagementapp/data/repository/EmployeeRepository.java
@@ -2,6 +2,12 @@ package com.alibknc.hrmanagementapp.data.repository;
 
 import com.alibknc.hrmanagementapp.data.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("select e from Employee e join fetch e.contactInfo c join fetch e.jobInfo j")
+    List<Employee> findAllEmployees();
+
 }
