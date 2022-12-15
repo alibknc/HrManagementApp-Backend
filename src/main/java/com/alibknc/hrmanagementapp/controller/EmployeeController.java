274227ecd39da04b332e828ec28f568121be684c
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:03:02 +0300

    deleteEmployee created

--- src/main/java/com/alibknc/hrmanagementapp/controller/EmployeeController.java
+++ src/main/java/com/alibknc/hrmanagementapp/controller/EmployeeController.java
@@ -2,10 +2,12 @@ package com.alibknc.hrmanagementapp.controller;
 
 import com.alibknc.hrmanagementapp.client.dto.response.EmployeeDto;
 import com.alibknc.hrmanagementapp.service.EmployeeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -26,5 +28,11 @@ public class EmployeeController {
     @PostMapping
     public ResponseEntity<Object> createEmployee(@Valid @RequestBody EmployeeDto employee) {
         return ResponseEntity.ok(employeeService.createEmployee(employee));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable UUID id) {
+        employeeService.deleteEmployee(id);
     }
 }
