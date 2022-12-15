4ad56877ace1d0337a8426b53a532b4d3e64670c
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 11:58:52 +0300

    long id changed with UUID. Employee variable added

--- src/main/java/com/alibknc/hrmanagementapp/data/entity/Permit.java
+++ src/main/java/com/alibknc/hrmanagementapp/data/entity/Permit.java
@@ -4,23 +4,31 @@ import com.alibknc.hrmanagementapp.data.entity.enums.PermitType;
 import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @Entity
 public class Permit {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    @GeneratedValue
+    @Column(length = 36)
+    UUID id;
 
-    Long employeeId;
-
+    @Column(nullable = false)
     LocalDate startDate;
 
+    @Column(nullable = false)
     LocalDate finishDate;
 
+    @Column(nullable = false)
     String details;
 
+    @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     PermitType permitType;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    Employee employee;
 }
