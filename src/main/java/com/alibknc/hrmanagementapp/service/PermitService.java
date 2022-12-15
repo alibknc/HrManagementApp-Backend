95e4dac46db171b2402b3ed98bee1a8c2eef42ba
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:01:07 +0300

    createPermit updated and deletePermit created

--- src/main/java/com/alibknc/hrmanagementapp/service/PermitService.java
+++ src/main/java/com/alibknc/hrmanagementapp/service/PermitService.java
@@ -1,13 +1,18 @@
 package com.alibknc.hrmanagementapp.service;
 
-import com.alibknc.hrmanagementapp.client.dto.response.PermitDto;
 import com.alibknc.hrmanagementapp.client.dto.mapper.PermitMapper;
+import com.alibknc.hrmanagementapp.client.dto.request.CreatePermitRequest;
+import com.alibknc.hrmanagementapp.client.dto.response.PermitDto;
+import com.alibknc.hrmanagementapp.data.entity.Employee;
 import com.alibknc.hrmanagementapp.data.entity.Permit;
+import com.alibknc.hrmanagementapp.data.repository.EmployeeRepository;
 import com.alibknc.hrmanagementapp.data.repository.PermitRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -17,17 +22,26 @@ public class PermitService {
 
     private final PermitRepository permitRepository;
     private final PermitMapper permitMapper;
+    private final EmployeeRepository employeeRepository;
 
+    @Transactional(readOnly = true)
     public List<PermitDto> getAllPermits() {
-        List<Permit> permitList = permitRepository.findAll();
+        List<Permit> permitList = permitRepository.findAllPermits();
 
         return permitList.stream()
                 .map(permitMapper::toPermitDto)
                 .collect(Collectors.toList());
     }
 
-    public PermitDto createPermit(PermitDto permit) {
-        permitRepository.save(permitMapper.toPermit(permit));
-        return permit;
+    public void createPermit(CreatePermitRequest request) {
+        Permit permit = permitMapper.toPermit(request);
+        Employee employee = employeeRepository.findById(request.getEmployeeId())
+                .orElseThrow(() -> new RuntimeException("employee not found"));
+        permit.setEmployee(employee);
+        permitRepository.save(permit);
+    }
+
+    public void deletePermit(UUID id) {
+        permitRepository.deleteById(id);
     }
 }
