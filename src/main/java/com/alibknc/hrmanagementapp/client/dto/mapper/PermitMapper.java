0aa87b366da3df8c1c7d49b60e30e1ca99e51202
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:05:54 +0300

    toPermit updated

--- src/main/java/com/alibknc/hrmanagementapp/client/dto/mapper/PermitMapper.java
+++ src/main/java/com/alibknc/hrmanagementapp/client/dto/mapper/PermitMapper.java
@@ -1,13 +1,16 @@
 package com.alibknc.hrmanagementapp.client.dto.mapper;
 
+import com.alibknc.hrmanagementapp.client.dto.request.CreatePermitRequest;
 import com.alibknc.hrmanagementapp.client.dto.response.PermitDto;
 import com.alibknc.hrmanagementapp.data.entity.Permit;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(implementationName = "PermitMapperImpl", componentModel = "spring")
+@Mapper(implementationName = "PermitMapperImpl", componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PermitMapper {
 
     PermitDto toPermitDto(Permit permit);
 
-    Permit toPermit(PermitDto permitDto);
+    Permit toPermit(CreatePermitRequest permitRequest);
+
 }
