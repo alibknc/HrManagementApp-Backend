05a639093192c3b5be8e2895524a44b63a040527
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:04:26 +0300

    long id changed with UUID

--- src/main/java/com/alibknc/hrmanagementapp/data/repository/EmployeeRepository.java
+++ src/main/java/com/alibknc/hrmanagementapp/data/repository/EmployeeRepository.java
@@ -4,8 +4,9 @@ import com.alibknc.hrmanagementapp.data.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
+import java.util.UUID;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
 
     @Query("select e from Employee e join fetch e.contactInfo c join fetch e.jobInfo j")
     List<Employee> findAllEmployees();
