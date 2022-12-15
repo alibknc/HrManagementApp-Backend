106faad239b10f8aac818c5c376c45d40ac0a837
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:01:25 +0300

    createPermit updated and deletePermit created

--- src/main/java/com/alibknc/hrmanagementapp/controller/PermitController.java
+++ src/main/java/com/alibknc/hrmanagementapp/controller/PermitController.java
@@ -1,11 +1,14 @@
 package com.alibknc.hrmanagementapp.controller;
 
+import com.alibknc.hrmanagementapp.client.dto.request.CreatePermitRequest;
 import com.alibknc.hrmanagementapp.client.dto.response.PermitDto;
 import com.alibknc.hrmanagementapp.service.PermitService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -24,7 +27,15 @@ public class PermitController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> createPermit(@Valid @RequestBody PermitDto permit) {
-        return ResponseEntity.ok(permitService.createPermit(permit));
+    public ResponseEntity<Void> createPermit(@Valid @RequestBody CreatePermitRequest permit) {
+        permitService.createPermit(permit);
+        return ResponseEntity.ok().build();
+    }
+
+    @CrossOrigin("http://localhost:3000")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deletePermit(@PathVariable UUID id) {
+        permitService.deletePermit(id);
     }
 }
