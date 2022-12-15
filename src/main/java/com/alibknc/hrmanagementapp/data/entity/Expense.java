298f2633d66364a076b8b20401b8c76011fc8e5e
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 11:58:16 +0300

    long id changed with UUID. Employee variable added

--- src/main/java/com/alibknc/hrmanagementapp/data/entity/Expense.java
+++ src/main/java/com/alibknc/hrmanagementapp/data/entity/Expense.java
@@ -4,25 +4,34 @@ import com.alibknc.hrmanagementapp.data.entity.enums.ExpenseType;
 import lombok.Data;
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @Entity
 public class Expense {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    @GeneratedValue
+    @Column(length = 36)
+    UUID id;
 
-    Long employeeId;
-
+    @Column(nullable = false)
     int expenseAmount;
 
+    @Column(nullable = false)
     int vat;
 
+    @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
     ExpenseType expenseType;
 
+    @Column(nullable = false)
     String details;
 
+    @Column(nullable = false)
     LocalDate expenseDate;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    Employee employee;
 }
