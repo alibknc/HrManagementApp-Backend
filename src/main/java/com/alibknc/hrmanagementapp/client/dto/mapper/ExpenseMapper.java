a6fef56fb05f9a7b987cefcf85d33048802fc061
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:06:17 +0300

    toExpense updated

--- src/main/java/com/alibknc/hrmanagementapp/client/dto/mapper/ExpenseMapper.java
+++ src/main/java/com/alibknc/hrmanagementapp/client/dto/mapper/ExpenseMapper.java
@@ -1,13 +1,16 @@
 package com.alibknc.hrmanagementapp.client.dto.mapper;
 
+import com.alibknc.hrmanagementapp.client.dto.request.CreateExpenseRequest;
 import com.alibknc.hrmanagementapp.client.dto.response.ExpenseDto;
 import com.alibknc.hrmanagementapp.data.entity.Expense;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(implementationName = "ExpenseMapperImpl", componentModel = "spring")
+@Mapper(implementationName = "ExpenseMapperImpl", componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ExpenseMapper {
 
     ExpenseDto toExpenseDto(Expense expense);
 
-    Expense toExpense(ExpenseDto expenseDto);
+    Expense toExpense(CreateExpenseRequest expense);
+
 }
