cb1e3dee338b901e90c1e18204a97bae92699175
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:05:16 +0300

    findAll query created

--- src/main/java/com/alibknc/hrmanagementapp/data/repository/ExpenseRepository.java
+++ src/main/java/com/alibknc/hrmanagementapp/data/repository/ExpenseRepository.java
@@ -2,6 +2,13 @@ package com.alibknc.hrmanagementapp.data.repository;
 
 import com.alibknc.hrmanagementapp.data.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.UUID;
 
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+public interface ExpenseRepository extends JpaRepository<Expense, UUID> {
+
+    @Query("select x from Expense x join fetch x.employee e")
+    List<Expense> findAllExpenses();
+
 }
--- src/main/java/com/alibknc/hrmanagementapp/data/repository/PermitRepository.java
+++ src/main/java/com/alibknc/hrmanagementapp/data/repository/PermitRepository.java
@@ -2,6 +2,13 @@ package com.alibknc.hrmanagementapp.data.repository;
 
 import com.alibknc.hrmanagementapp.data.entity.Permit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.UUID;
 
-public interface PermitRepository extends JpaRepository<Permit, Long> {
+public interface PermitRepository extends JpaRepository<Permit, UUID> {
+
+    @Query("select p from Permit p join fetch p.employee e")
+    List<Permit> findAllPermits();
+
 }
