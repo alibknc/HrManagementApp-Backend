e210dfff3fbd0cb010a1e6a63d4442215f170294
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:11:13 +0300

    createExpense updated. deleteExpense created

--- src/main/java/com/alibknc/hrmanagementapp/controller/ExpenseController.java
+++ src/main/java/com/alibknc/hrmanagementapp/controller/ExpenseController.java
@@ -1,11 +1,14 @@
 package com.alibknc.hrmanagementapp.controller;
 
+import com.alibknc.hrmanagementapp.client.dto.request.CreateExpenseRequest;
 import com.alibknc.hrmanagementapp.client.dto.response.ExpenseDto;
 import com.alibknc.hrmanagementapp.service.ExpenseService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -24,7 +27,15 @@ public class ExpenseController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> createExpense(@Valid @RequestBody ExpenseDto expense) {
-        return ResponseEntity.ok(expenseService.createExpense(expense));
+    public ResponseEntity<Void> createExpense(@Valid @RequestBody CreateExpenseRequest expense) {
+        expenseService.createExpense(expense);
+        return ResponseEntity.ok().build();
+    }
+
+    @CrossOrigin("http://localhost:3000")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteExpense(@PathVariable UUID id) {
+        expenseService.deleteExpense(id);
     }
 }
