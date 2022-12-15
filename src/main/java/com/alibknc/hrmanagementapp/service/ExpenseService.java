1d4e2f9e7801ffa5ee05112a7f14fb2e94216617
Muhammet Ali Bakınç <mss_258852@hotmail.com>
2022-12-15 12:10:40 +0300

    getAllExpenses, createExpense updated. deleteExpense created

--- src/main/java/com/alibknc/hrmanagementapp/service/ExpenseService.java
+++ src/main/java/com/alibknc/hrmanagementapp/service/ExpenseService.java
@@ -1,13 +1,18 @@
 package com.alibknc.hrmanagementapp.service;
 
-import com.alibknc.hrmanagementapp.client.dto.response.ExpenseDto;
 import com.alibknc.hrmanagementapp.client.dto.mapper.ExpenseMapper;
+import com.alibknc.hrmanagementapp.client.dto.request.CreateExpenseRequest;
+import com.alibknc.hrmanagementapp.client.dto.response.ExpenseDto;
+import com.alibknc.hrmanagementapp.data.entity.Employee;
 import com.alibknc.hrmanagementapp.data.entity.Expense;
+import com.alibknc.hrmanagementapp.data.repository.EmployeeRepository;
 import com.alibknc.hrmanagementapp.data.repository.ExpenseRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -15,18 +20,27 @@ import java.util.stream.Collectors;
 @Service
 public class ExpenseService {
     private final ExpenseRepository expenseRepository;
+    private final EmployeeRepository employeeRepository;
     private final ExpenseMapper expenseMapper;
 
+    @Transactional(readOnly = true)
     public List<ExpenseDto> getAllExpenses() {
-        List<Expense> expenseList = expenseRepository.findAll();
+        List<Expense> expenseList = expenseRepository.findAllExpenses();
 
         return expenseList.stream()
                 .map(expenseMapper::toExpenseDto)
                 .collect(Collectors.toList());
     }
 
-    public ExpenseDto createExpense(ExpenseDto expense) {
-        expenseRepository.save(expenseMapper.toExpense(expense));
-        return expense;
+    public void createExpense(CreateExpenseRequest request) {
+        Expense expense = expenseMapper.toExpense(request);
+        Employee employee = employeeRepository.findById(request.getEmployeeId())
+                .orElseThrow(() -> new RuntimeException("employee not found"));
+        expense.setEmployee(employee);
+        expenseRepository.save(expense);
+    }
+
+    public void deleteExpense(UUID id) {
+        expenseRepository.deleteById(id);
     }
 }
